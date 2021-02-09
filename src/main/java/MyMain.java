34fc48a34f7214751c789326089832d58f0cf82e
BrunBoy <23rreddy@athenian.org>
2021-02-09 01:52:23 +0000

    oH

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -27,14 +27,22 @@ public class MyMain {
      // Wrapper method
     public static boolean hasCountCopies(int[] arr, int x, int count) {
         // YOUR CODE HERE
-        return hasCountCopiesRec(arr, x, count, 0);
+        return hasCountCopiesRec(arr, x, count, 0, 0);
     }
 
     // This recursive method checks if the array contains exactly
     // count copies of the integer x
-    public static boolean hasCountCopiesRec(int[] arr, int x, int count, int i) {
+    public static boolean hasCountCopiesRec(int[] arr, int x, int count, int i, int countX) {
         // YOUR CODE HERE
-        return false;
+       if (i==arr.length){
+           return (count==countX);
+       }
+       if (arr[i]==x){
+           return hasCountCopiesRec(arr, x, count, i+1, countX+1);
+       }
+       else{
+           return hasCountCopiesRec(arr, x, count, i+1, countX);          
+       }
     }
 
 
@@ -43,13 +51,26 @@ public class MyMain {
     // Wrapper method
     public static boolean binarySearch(int[] arr, int num) {
         // YOUR CODE HERE
-        return false;
+        return binarySearchRec(arr, num, 0, arr.length-1);
     }
 
     // This recursive method calls binary search on the array
     public static boolean binarySearchRec(int[] arr, int num, int lowerBound, int upperBound) {        
         // YOUR CODE HERE
-        return false;
+        if (lowerBound==upperBound-1){
+            return false; 
+        }
+        if (arr[(lowerBound+upperBound)/2]==num){
+            return true;
+        }
+        if (arr[(lowerBound+upperBound)/2]<num){
+            lowerBound=(lowerBound+upperBound)/2;
+            return binarySearchRec(arr, num, lowerBound, upperBound);
+        }
+        else{
+            upperBound=(lowerBound+upperBound)/2;
+            return binarySearchRec(arr, num, lowerBound, upperBound);
+        }
     }
 
     public static void main(String[] args) {
